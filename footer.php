b9f4b60d1edd56a4ea1cad91976483d80b7f9471
Ikaro <ikarovieiraa@gmail.com>
2022-02-11 02:38:02 -0300

    implementação do footer

---
+++ footer.php
@@ -0,0 +1,23 @@
+<footer class="footer px-2 px-sm-3 px-lg-5 py-1 py-sm-2 py-lg-3 d-flex w-100 align-items-center">
+    <span class="d-flex align-items-center flex-shrink-1 flex-grow-1 justify-content-start">
+        <a href="index.php" aria-current="page" aria-label="DoYou" class="pl-1 DoYou footer-link">
+            DoYou <span>- &copy; Copyright 2022</span>
+        </a> 
+    </span>
+    <div class="d-block justify-content-end">
+        <div class="position-relative d-inline-block">
+            <div class="align-items-center d-flex">
+                <a href="" class="rede-social">
+                    <img src="img/wpp.png" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="position-relative d-inline-block">
+            <div class="align-items-center d-flex">
+                <a href="" class="rede-social">
+                    <img src="img/tw.png" alt="">
+                </a>
+            </div>
+        </div>
+    </div>
+</footer>
