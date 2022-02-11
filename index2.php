ee87fd46edd915a4752f0c13a568dbcab11d85c5
Ikaro <ikarovieiraa@gmail.com>
2022-02-11 02:37:31 -0300

    Atualização do index

---
+++ index2.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DoYou | Educação Financeira</title>
+
+    <!-- DoYou Style -->
+    <link rel="stylesheet" href="css/style-header.css">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+</head>
+<body>
+    <!-- CABEÇALHO -->
+        <?php include_once "header2.php"; ?>
+    <!--  -->
+
+    <!-- CARROSSEL -->
+        <img src="img/ft1.png" class="img-fluid" alt="">
+    <!--  -->
+
+    <h3 class="w-100 my-5 py-1 h3DoYou text-center">Sobre nós</h3>
+
+    <section class="container d-flex text-center justify-content-center">
+        <div class="row justify-content-md-around flex-md-row flex-column w-100">
+            <div class="texto-sobre mb-3">
+                <h5 class="d-inline-block px-3 py-1">Missão</h5>
+                <h4>Promover a educação<br>financeira</h4>
+            </div>
+            <div class="texto-sobre mb-3">
+                <h5 class="d-inline-block px-3 py-1">Serviços</h5>
+                <h4>Finaceiros</h4>
+            </div>
+            <div class="texto-sobre mb-3">
+                <h5 class="d-inline-block px-3 py-1">Industria</h5>
+                <h4>Serviços Finaceiros</h4>
+            </div>
+        </div>
+    </section>
+
+    <!-- FOOTER -->
+        <?php include_once "footer.php" ?>
+    <!--  -->
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+</body>
+</html>
